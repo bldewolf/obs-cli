751809b4647422df60ca66117f550b6e085b19d7
Brian De Wolf <git@bldewolf.com>
2022-04-05 13:50:57 -0700

    add stdin option

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"bufio"
+	"strings"
 
 	"github.com/andreykaipov/goobs"
 	"github.com/muesli/coral"
@@ -18,6 +20,22 @@ var (
 	rootCmd = &coral.Command{
 		Use:   "obs-cli",
 		Short: "obs-cli is a command-line remote control for OBS",
+	}
+
+	stdinCmd = &coral.Command{
+		Use:   "stdin",
+		Short: "Reads lines on stdin and executes each as a command",
+		RunE: func(cmd *coral.Command, args []string) error {
+			scanner := bufio.NewScanner(os.Stdin)
+			for scanner.Scan() {
+				os.Args = append([]string{os.Args[0]},strings.Split(scanner.Text(), " ")...)
+				rootCmd.Execute()
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Println(err)
+			}
+			return nil
+		},
 	}
 
 	client *goobs.Client
@@ -39,6 +57,7 @@ func init() {
 	rootCmd.PersistentFlags().StringVar(&host, "host", "localhost", "host to connect to")
 	rootCmd.PersistentFlags().StringVar(&password, "password", "", "password for connection")
 	rootCmd.PersistentFlags().Uint32VarP(&port, "port", "p", 4444, "port to connect to")
+	rootCmd.AddCommand(stdinCmd)
 }
 
 func getUserAgent() string {
@@ -51,13 +70,15 @@ func getUserAgent() string {
 
 func connectOBS() {
 	var err error
-	client, err = goobs.New(
-		host+fmt.Sprintf(":%d", port),
-		goobs.WithPassword(password),
-		goobs.WithRequestHeader(http.Header{"User-Agent": []string{getUserAgent()}}),
-	)
-	if err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
+	if client == nil {
+		client, err = goobs.New(
+			host+fmt.Sprintf(":%d", port),
+			goobs.WithPassword(password),
+			goobs.WithRequestHeader(http.Header{"User-Agent": []string{getUserAgent()}}),
+		)
+		if err != nil {
+			fmt.Println("error:", err)
+			os.Exit(1)
+		}
 	}
 }
